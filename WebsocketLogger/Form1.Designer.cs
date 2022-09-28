22db41aa05a2f042994f6ffd18e93ecd052c3a14
marsmaantje[pc] <caspersikkens@planet.nl>
2022-09-28 17:00:18 +0200

    Initial commit

---
+++ WebsocketLogger/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RP3_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.MessageText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MessageText
+            // 
+            this.MessageText.AutoSize = true;
+            this.MessageText.Location = new System.Drawing.Point(551, 249);
+            this.MessageText.Name = "MessageText";
+            this.MessageText.Size = new System.Drawing.Size(35, 13);
+            this.MessageText.TabIndex = 0;
+            this.MessageText.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 865);
+            this.Controls.Add(this.MessageText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label MessageText;
+    }
+}
+
---
+++ WebsocketLogger/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Fleck;
+
+
+namespace RP3_Interface
+{
+    public partial class Form1 : Form
+    {
+        public static bool formOpen = true;
+        string lastMessage = "";
+        WebSocketServer wss;
+        List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
+
+
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            Console.WriteLine("hello world");
+
+            Timer updateClock = new Timer();
+            updateClock.Interval = 1000 / 10;
+            updateClock.Tick += Update;
+            updateClock.Start();
+
+            wss = new WebSocketServer("ws://127.0.0.1:2071");
+            wss.Start( socket =>
+            {
+                socket.OnOpen = () => { allSockets.Add(socket); };
+                socket.OnClose = () => { allSockets.Remove(socket); };
+                socket.OnMessage += OnMessage;
+            });
+            
+        }
+
+        void OnMessage(string message)
+        {
+            lastMessage = message;
+        }
+
+        void formClosing(object sender, EventArgs e)
+        {
+            wss.Dispose();
+            formOpen = false;
+        }
+
+        /// <summary>
+        /// Update method, gets run on a loop while the window is open.
+        /// </summary>
+        private void Update(object sender, EventArgs e)
+        {
+            MessageText.Text = lastMessage;
+        }
+    }
+}
